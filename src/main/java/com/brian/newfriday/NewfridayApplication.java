5787fe1885d0dcbaa498512a417c2795c0ec47e9
brianrego03 <brian.rego03@gmail.com>
2025-11-22 10:10:52 +0530

    Initial commit

---
+++ src/main/java/com/brian/newfriday/NewfridayApplication.java
@@ -0,0 +1,13 @@
+package com.brian.newfriday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewfridayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewfridayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brian/newfriday/NewfridayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brian.newfriday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewfridayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
