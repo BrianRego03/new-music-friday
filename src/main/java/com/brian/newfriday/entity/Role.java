731cf3164efa1127df68a651ec734acc50cedd63
brianrego03 <brian.rego03@gmail.com>
2025-11-25 16:25:19 +0530

    Create Enum for Role and add constructors to user class

---
+++ src/main/java/com/brian/newfriday/entity/Role.java
@@ -0,0 +1,6 @@
+package com.brian.newfriday.entity;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- src/main/java/com/brian/newfriday/entity/User.java
+++ src/main/java/com/brian/newfriday/entity/User.java
@@ -1,6 +1,9 @@
 package com.brian.newfriday.entity;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name="users")
@@ -19,5 +22,21 @@ public class User {
     @Column(name="password")
     private String password;
 
+    @Column(name="role")
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name="create_time",updatable = false,nullable = false)
+    @CreationTimestamp
+    private LocalDate createTime;
+
+    public User(){}
+
+    public User(String name, String email,String password){
+        this.name=name;
+        this.email=email;
+        this.password=password;
+        this.createTime=LocalDate.now();
+    }
 
 }
