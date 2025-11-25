d9fdd465f8e3612249774edc418a06a5dcd92714
brianrego03 <brian.rego03@gmail.com>
2025-11-25 15:07:48 +0530

    Create User entity class

---
+++ src/main/java/com/brian/newfriday/entity/User.java
@@ -0,0 +1,23 @@
+package com.brian.newfriday.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="password")
+    private String password;
+
+
+}
