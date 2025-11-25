942d475d902d6b580f714a88248e06143a49792d
brianrego03 <brian.rego03@gmail.com>
2025-11-25 17:40:48 +0530

    Create getter and setter methods for User class

--- src/main/java/com/brian/newfriday/entity/User.java
+++ src/main/java/com/brian/newfriday/entity/User.java
@@ -39,4 +39,20 @@ public class User {
         this.createTime=LocalDate.now();
     }
 
+    public String getName(){return this.name;}
+
+    public void setName(String name){this.name=name;}
+
+    public String getEmail(){return this.email;}
+
+    public void setEmail(String email){this.email=email;}
+
+    public String getPassword(){return this.password;}
+
+    public void setPassword(String password){this.password=password;}
+
+    public Role getRole(){return this.role;}
+
+    public void setRole(Role role){this.role=role;}
+
 }
