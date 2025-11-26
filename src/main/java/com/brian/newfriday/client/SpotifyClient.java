08c5283c032c6e29c5a48ba620b0c6855ab2b68d
brianrego03 <brian.rego03@gmail.com>
2025-11-26 14:43:32 +0530

    Write an Execution method to refresh token and retry the request

--- src/main/java/com/brian/newfriday/client/SpotifyClient.java
+++ src/main/java/com/brian/newfriday/client/SpotifyClient.java
@@ -1,7 +1,11 @@
 package com.brian.newfriday.client;
 
 import com.brian.newfriday.service.SpotifyTokenService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClient;
+
+import java.util.function.Supplier;
 
 public class SpotifyClient {
     private final RestClient restClient;
@@ -10,6 +14,23 @@ public class SpotifyClient {
     public SpotifyClient(RestClient restClient,SpotifyTokenService spotifyTokenService){
         this.restClient=restClient;
         this.spotifyTokenService=spotifyTokenService;
+    }
+
+//    public void getUser(Long id){
+//
+//    }
+
+    private <T> T executeWithTokenRetry(Supplier<T> request){
+        try{
+            return request.get();
+        }catch(HttpClientErrorException e){
+            if(e.getStatusCode()== HttpStatus.UNAUTHORIZED){
+                spotifyTokenService.refreshToken();
+                return request.get();
+            }else{
+                throw e;
+            }
+        }
     }
 
 
