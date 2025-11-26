8866d01d1cc39e5d71cb979ed5352017a0f87870
brianrego03 <brian.rego03@gmail.com>
2025-11-26 13:06:09 +0530

    Initialize token services and constructor for spotify client

--- src/main/java/com/brian/newfriday/client/SpotifyClient.java
+++ src/main/java/com/brian/newfriday/client/SpotifyClient.java
@@ -1,5 +1,16 @@
 package com.brian.newfriday.client;
 
+import com.brian.newfriday.service.SpotifyTokenService;
+import org.springframework.web.client.RestClient;
+
 public class SpotifyClient {
+    private final RestClient restClient;
+    private final SpotifyTokenService spotifyTokenService;
+
+    public SpotifyClient(RestClient restClient,SpotifyTokenService spotifyTokenService){
+        this.restClient=restClient;
+        this.spotifyTokenService=spotifyTokenService;
+    }
+
 
 }
