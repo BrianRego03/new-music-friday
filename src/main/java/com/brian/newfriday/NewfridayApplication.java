dadc16c065e94e06f237f2e947c1e6f34b56282d
brianrego03 <brian.rego03@gmail.com>
2025-11-26 15:03:11 +0530

    Create a method to fetch Artist details from Spotify

--- src/main/java/com/brian/newfriday/NewfridayApplication.java
+++ src/main/java/com/brian/newfriday/NewfridayApplication.java
@@ -1,5 +1,6 @@
 package com.brian.newfriday;
 
+import com.brian.newfriday.client.SpotifyClient;
 import com.brian.newfriday.service.SpotifyTokenService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,8 @@ public class NewfridayApplication {
         System.out.println("Hi");
         var servicing = context.getBean(SpotifyTokenService.class);
 //        System.out.println(servicing.fetchSpotifyToken());
+        var altService = context.getBean(SpotifyClient.class);
+//        altService.getArtist("1oPRcJUkloHaRLYx0olBLJ");
 	}
 
 }
--- src/main/java/com/brian/newfriday/client/SpotifyClient.java
+++ src/main/java/com/brian/newfriday/client/SpotifyClient.java
@@ -1,24 +1,41 @@
 package com.brian.newfriday.client;
 
 import com.brian.newfriday.service.SpotifyTokenService;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClient;
 
 import java.util.function.Supplier;
 
+@Service
 public class SpotifyClient {
     private final RestClient restClient;
     private final SpotifyTokenService spotifyTokenService;
+    private final String baseUrl = "https://api.spotify.com/v1/";
 
     public SpotifyClient(RestClient restClient,SpotifyTokenService spotifyTokenService){
         this.restClient=restClient;
         this.spotifyTokenService=spotifyTokenService;
     }
 
-//    public void getUser(Long id){
-//
-//    }
+    public void getArtist(String id){
+        if(id==null){
+            return;
+        }
+        var user = executeWithTokenRetry(()->restClient.get()
+                .uri(baseUrl + "artists/" + id)
+                .header("Authorization", "Bearer " + spotifyTokenService.getSpotifyToken())
+                .retrieve()
+                .body(JsonNode.class)
+
+        );
+
+        System.out.println(user.get("name"));
+        System.out.println(user.get("href"));
+        return;
+    }
 
     private <T> T executeWithTokenRetry(Supplier<T> request){
         try{
