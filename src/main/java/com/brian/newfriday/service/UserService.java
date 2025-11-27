ce245b225bcd81508307e1e653f143c8dc3abfb0
brianrego03 <brian.rego03@gmail.com>
2025-11-27 16:55:32 +0530

    Create a method to fetch all users

--- src/main/java/com/brian/newfriday/service/UserService.java
+++ src/main/java/com/brian/newfriday/service/UserService.java
@@ -3,7 +3,10 @@ package com.brian.newfriday.service;
 import com.brian.newfriday.entity.User;
 import com.brian.newfriday.repository.UserRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -18,6 +21,10 @@ public class UserService {
 
         User usernew = new User(name,email,password);
         return userRepository.save(usernew);
+    }
+
+    public List<User> getAllUsers(String sort){
+        return userRepository.findAll(Sort.by(sort));
     }
 
 }
