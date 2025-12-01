3ea12717a5c0159948d9aa2447d2fa28ddb84516
brianrego03 <brian.rego03@gmail.com>
2025-12-01 15:07:56 +0530

    Create service methods to create and delete artists

--- src/main/java/com/brian/newfriday/service/ArtistService.java
+++ src/main/java/com/brian/newfriday/service/ArtistService.java
@@ -3,7 +3,10 @@ package com.brian.newfriday.service;
 import com.brian.newfriday.entity.Artist;
 import com.brian.newfriday.repository.ArtistRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ArtistService {
@@ -17,6 +20,20 @@ public class ArtistService {
     public Artist registerArtist(String name, String spotifyID, String imgSmall, String imgMedium, String imgLarge){
         Artist newArtist = new Artist(name, spotifyID, imgSmall, imgMedium, imgLarge);
         return artistRepository.save(newArtist);
+    }
+
+    public Artist getArtistById(int id){
+        return artistRepository.findById(id).orElse(null);
+    }
+
+    public List<Artist> getAllArtists(String sort){
+        return artistRepository.findAll(Sort.by(sort));
+    }
+
+    @Transactional
+    public String DeleteArtist(int id){
+        artistRepository.deleteById(id);
+        return "Success";
     }
 
 
