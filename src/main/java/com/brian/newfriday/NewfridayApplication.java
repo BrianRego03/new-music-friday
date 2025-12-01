7cd78a63650dc47ef9fc984dc11e292cb203d1e0
brianrego03 <brian.rego03@gmail.com>
2025-12-01 15:32:32 +0530

    Test the artist service methods

--- src/main/java/com/brian/newfriday/NewfridayApplication.java
+++ src/main/java/com/brian/newfriday/NewfridayApplication.java
@@ -1,6 +1,7 @@
 package com.brian.newfriday;
 
 import com.brian.newfriday.client.SpotifyClient;
+import com.brian.newfriday.service.ArtistService;
 import com.brian.newfriday.service.SpotifyTokenService;
 import com.brian.newfriday.service.UserService;
 import org.springframework.boot.SpringApplication;
@@ -21,7 +22,14 @@ public class NewfridayApplication {
 //        altService.getArtistInclusive("2gsggkzM5R49q6jpPvazou");
 //        altService.searchArtist("magdalena bay");
         var userServicer = context.getBean(UserService.class);
-        userServicer.registerUser("Tina","tina1717@gmail.com","funnystuff");
+//        userServicer.registerUser("Tina","tina1717@gmail.com","funnystuff");
+        var artistServicer = context.getBean(ArtistService.class);
+//        artistServicer.registerArtist("Magdalena Bay","1oPRcJUkloHaRLYx0olBLJ",
+//                "https://i.scdn.co/image/ab6761610000f1788257ff04aaf62d848030efeb",
+//                "https://i.scdn.co/image/ab676161000051748257ff04aaf62d848030efeb",
+//                "https://i.scdn.co/image/ab6761610000e5eb8257ff04aaf62d848030efeb");
+//
+//        userServicer.DeleteUser(3);
 	}
 
 }
