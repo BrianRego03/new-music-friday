16f0c71db421b4849830d391cc1735721815da60
brianrego03 <brian.rego03@gmail.com>
2026-01-09 17:23:58 +0530

    Correct constructor and add register method

--- src/main/java/com/brian/newfriday/service/AlbumService.java
+++ src/main/java/com/brian/newfriday/service/AlbumService.java
@@ -1,14 +1,26 @@
 package com.brian.newfriday.service;
 
+import com.brian.newfriday.entity.Album;
+import com.brian.newfriday.entity.Record;
+import com.brian.newfriday.repository.AlbumRepository;
 import com.brian.newfriday.repository.ArtistRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public class AlbumService {
 
-    private final ArtistRepository artistRepository;
+    private final AlbumRepository albumRepository;
 
-    public AlbumService(ArtistRepository artistRepository){
-        this.artistRepository=artistRepository;
+    public AlbumService(AlbumRepository albumRepository){
+        this.albumRepository=albumRepository;
+    }
+
+    public Album registerAlbum(String title, String spotifyID, String imgSmall, String imgMedium, String imgLarge,
+                               int trackLength, LocalDate releaseDate, Record albumType){
+        Album newAlbum = new Album(title, spotifyID, imgSmall, imgMedium, imgLarge,
+                trackLength, releaseDate, albumType);
+        return albumRepository.save(newAlbum);
     }
 }
