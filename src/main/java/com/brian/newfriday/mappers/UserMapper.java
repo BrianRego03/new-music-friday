27e738ed4ee3e476e35d0ffbf9eecf4c60c5763f
Brian Dominic Rego <brian.rego03@gmail.com>
2026-01-13 06:45:15 +0530

    Create mapper for Users

---
+++ src/main/java/com/brian/newfriday/mappers/UserMapper.java
@@ -0,0 +1,10 @@
+package com.brian.newfriday.mappers;
+
+import com.brian.newfriday.dtos.UserDto;
+import com.brian.newfriday.entity.User;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    UserDto userToUserDto(User user);
+}
