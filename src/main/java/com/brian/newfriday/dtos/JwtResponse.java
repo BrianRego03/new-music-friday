5a90d1124ed4ce89b51545ffac9b53c57648159d
brianrego03 <brian.rego03@gmail.com>
2026-01-17 16:42:13 +0530

    Create dto for jwtresponse

---
+++ src/main/java/com/brian/newfriday/dtos/JwtResponse.java
@@ -0,0 +1,17 @@
+package com.brian.newfriday.dtos;
+
+public class JwtResponse {
+    private String token;
+
+    public JwtResponse(String token){
+        this.token=token;
+    }
+
+    public String getToken(){
+        return this.token;
+    }
+
+    public void setToken(String token){
+        this.token=token;
+    }
+}
