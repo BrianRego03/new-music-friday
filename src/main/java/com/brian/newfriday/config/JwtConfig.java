963134f044a1b33e85c298add480ba458d0a2e46
brianrego03 <brian.rego03@gmail.com>
2026-01-17 15:06:50 +0530

    Write configuration for JWT

---
+++ src/main/java/com/brian/newfriday/config/JwtConfig.java
@@ -0,0 +1,49 @@
+package com.brian.newfriday.config;
+
+import io.jsonwebtoken.security.Keys;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import javax.crypto.SecretKey;
+
+@Configuration
+@ConfigurationProperties(prefix = "app.jwt")
+public class JwtConfig {
+    private String secret;
+    private Long accessTokenExpiration;
+    private Long refreshTokenExpiration;
+
+    public JwtConfig(){
+
+    }
+
+    public void setSecret(String secret){
+        this.secret=secret;
+    }
+
+    public void setAccessTokenExpiration(Long accessTokenExpiration){
+        this.accessTokenExpiration=accessTokenExpiration;
+    }
+
+    public void setRefreshTokenExpiration(Long refreshTokenExpiration){
+        this.refreshTokenExpiration=refreshTokenExpiration;
+    }
+
+    public String getSecret(){
+        return this.secret;
+    }
+
+    public Long getAccessTokenExpiration(){
+        return this.accessTokenExpiration;
+    }
+
+    public Long getRefreshTokenExpiration(){
+        return this.refreshTokenExpiration;
+    }
+
+    public SecretKey getSecretKey(){
+        return Keys.hmacShaKeyFor(getSecret().getBytes());
+    }
+
+
+}
