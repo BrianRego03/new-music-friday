aa8c91a5cecaee2999fe21cb50deefd03437588a
brianrego03 <brian.rego03@gmail.com>
2026-01-19 17:42:38 +0530

    Create method to fetch user info

--- src/main/java/com/brian/newfriday/controller/AuthController.java
+++ src/main/java/com/brian/newfriday/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.brian.newfriday.controller;
 import com.brian.newfriday.config.JwtConfig;
 import com.brian.newfriday.dtos.CredentialsRequest;
 import com.brian.newfriday.dtos.JwtResponse;
+import com.brian.newfriday.dtos.UserDto;
 import com.brian.newfriday.mappers.UserMapper;
 import com.brian.newfriday.repository.UserRepository;
 import com.brian.newfriday.service.JwtService;
@@ -14,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -78,6 +80,18 @@ public class AuthController {
         var user = userRepository.findById(userId).orElseThrow();
         var accessToken = jwtService.generateAccessToken(user);
         return ResponseEntity.ok(new JwtResponse(accessToken));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> fetchMe(){
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        var userId = (Integer) authentication.getPrincipal();
+        var user = userService.getById(userId);
+        if(user==null){
+            return ResponseEntity.notFound().build();
+        }
+        var userDto = userMapper.toDto(user);
+        return ResponseEntity.ok(userDto);
     }
 
 
