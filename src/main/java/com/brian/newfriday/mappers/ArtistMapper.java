c367920e3c9e38e0ed50c6053e2df11fc59ab7a3
brianrego03 <brian.rego03@gmail.com>
2026-01-23 15:12:21 +0530

    Create mapper for artist and it's dto

---
+++ src/main/java/com/brian/newfriday/mappers/ArtistMapper.java
@@ -0,0 +1,11 @@
+package com.brian.newfriday.mappers;
+
+import com.brian.newfriday.dtos.SpotifyArtistDto;
+import com.brian.newfriday.entity.Artist;
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface ArtistMapper {
+    Artist toArtist(SpotifyArtistDto spotifyArtistDto);
+}
