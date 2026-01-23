736ba8d8097caf43999f47f7b00d3bd6269eaeba
brianrego03 <brian.rego03@gmail.com>
2026-01-23 16:01:42 +0530

    Create a basic controller method to get artist by spotify id

--- src/main/java/com/brian/newfriday/controller/ArtistController.java
+++ src/main/java/com/brian/newfriday/controller/ArtistController.java
@@ -2,6 +2,7 @@ package com.brian.newfriday.controller;
 
 import com.brian.newfriday.repository.ArtistRepository;
 import com.brian.newfriday.service.ArtistService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +13,11 @@ public class ArtistController {
     public ArtistController(ArtistRepository artistRepository, ArtistService artistService) {
         this.artistRepository = artistRepository;
         this.artistService = artistService;
+    }
+
+    @GetMapping("/artists/{spotifyId}" )
+    public void getArtistBySpotifyId(String spotifyId){
+        artistService.getArtistBySpotifyID(spotifyId);
+        return;
     }
 }
