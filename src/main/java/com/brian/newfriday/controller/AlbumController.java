49acebaf77bc470f5c08fff657b62e784ef2baf4
brianrego03 <brian.rego03@gmail.com>
2026-01-24 16:46:10 +0530

    Create controller method to get an album by spotify id

--- src/main/java/com/brian/newfriday/controller/AlbumController.java
+++ src/main/java/com/brian/newfriday/controller/AlbumController.java
@@ -1,28 +1,44 @@
 package com.brian.newfriday.controller;
 
+import com.brian.newfriday.dtos.AlbumResponseDto;
+import com.brian.newfriday.dtos.SpotifyAlbumDto;
+import com.brian.newfriday.dtos.SpotifyArtistDto;
+import com.brian.newfriday.entity.Album;
+import com.brian.newfriday.entity.Artist;
 import com.brian.newfriday.mappers.AlbumMapper;
+import com.brian.newfriday.mappers.ArtistMapper;
 import com.brian.newfriday.repository.AlbumRepository;
 import com.brian.newfriday.service.AlbumService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class AlbumController {
     private AlbumService albumService;
     private AlbumRepository albumRepository;
     private AlbumMapper albumMapper;
+    private ArtistMapper artistMapper;
 
     public AlbumController(AlbumService albumService, AlbumRepository albumRepository,
-                           AlbumMapper albumMapper){
+                           AlbumMapper albumMapper, ArtistMapper artistMapper){
         this.albumService = albumService;
         this.albumRepository = albumRepository;
         this.albumMapper = albumMapper;
+        this.artistMapper = artistMapper;
     }
 
     @GetMapping("/albums/{SpotifyId}")
-    public void getAlbumBySpotifyId(@PathVariable String SpotifyId){
-        albumRepository.findBySpotifyID(SpotifyId);
+    public AlbumResponseDto getAlbumBySpotifyId(@PathVariable String SpotifyId){
+        Album album = albumRepository.findBySpotifyID(SpotifyId);
+        List<Artist> artists = album.getArtistsAsList();
+        SpotifyAlbumDto albumDto = albumMapper.toDto(album);
+        List<SpotifyArtistDto> artistDtos = artists.stream()
+                .map(artistMapper::toSpotifyDto)
+                .toList();
+        return new AlbumResponseDto(albumDto, artistDtos);
 
 
     }
