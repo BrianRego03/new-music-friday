ed991e72fd4c6c30845ec96c904dc2329887b47b
brianrego03 <brian.rego03@gmail.com>
2026-01-24 16:09:45 +0530

    Create repo method to fetch albums by user and time window

--- src/main/java/com/brian/newfriday/repository/UserRepository.java
+++ src/main/java/com/brian/newfriday/repository/UserRepository.java
@@ -1,9 +1,33 @@
 package com.brian.newfriday.repository;
 
+import com.brian.newfriday.entity.Album;
 import com.brian.newfriday.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
     User findByEmail(String email);
     boolean existsByEmail(String email);
+
+    @Query(
+            """
+                    SELECT a from Album a
+                    JOIN a.artistSet artist
+                    JOIN artist.userSet user
+                    WHERE user.id = :userId
+                    AND (:startDate IS NULL OR a.releaseDate >= :startDate)
+                    AND (:endDate IS NULL OR a.releaseDate <= :endDate)
+                    ORDER BY a.releaseDate DESC
+                  
+                    """
+    )
+    List<Album> getAlbumsByUserIdAndReleaseDateBetween(
+            @Param("userId") int userId,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate
+    );
 }
