7a71a41171541dd38b493c702401d9a34bbdeaac
Brian Dominic Rego <brian.rego03@gmail.com>
2026-01-29 23:26:16 +0530

    Add some print statements to observe refresh process

--- src/main/java/com/brian/newfriday/service/ArtistRefreshService.java
+++ src/main/java/com/brian/newfriday/service/ArtistRefreshService.java
@@ -33,25 +33,31 @@ public class ArtistRefreshService {
 
         try{
             List<ArtistLatestDto> latestArtists = artistRepository.getLatestArtistsWithAlbums();
+            System.out.println("Listed successfully");
             for(ArtistLatestDto artistLatestDto : latestArtists){
                 while(paused.get()){
                     Thread.sleep(1000);
                 }
                 String artistID = artistLatestDto.getArtistId();
+                System.out.println("refreshing Artist ID: " + artistID);
                 String latestAlbumID = artistLatestDto.getLatestAlbumId();
                 String apiLatestAlbumID = spotifyClient.getLatestAlbumByArtistSpotifyId(artistID);
+                System.out.println("Not api rated yet, taking a break");
                 Thread.sleep(1*3*1000);//3 sec break
                 if(apiLatestAlbumID==null || apiLatestAlbumID.equals(latestAlbumID)){
                     continue;
                 }
                 artistService.getCompleteArtist(artistID);
+                System.out.println("next api success");
 
             }
             return CompletableFuture.completedFuture(null);
         }catch (Exception e){
             //log.error("Refresh failed", e);
+            System.out.println("Error in refreshArtistData");
             return CompletableFuture.completedFuture(null);
         }finally {
+            System.out.println("Artist refresh complete");
             running.set(false);
         }
     }
