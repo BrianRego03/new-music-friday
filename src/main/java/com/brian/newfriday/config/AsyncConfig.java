24a2ec60a89af52dd09a222b00752d8e09f1ad8f
brianrego03 <brian.rego03@gmail.com>
2026-01-29 18:23:06 +0530

    Add bean annotation

--- src/main/java/com/brian/newfriday/config/AsyncConfig.java
+++ src/main/java/com/brian/newfriday/config/AsyncConfig.java
@@ -1,5 +1,6 @@
 package com.brian.newfriday.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
@@ -7,7 +8,7 @@ import java.util.concurrent.Executor;
 
 @Configuration
 public class AsyncConfig {
-
+    @Bean
     public Executor refreshExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(1);
