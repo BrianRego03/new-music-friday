b77681557fec77ecae73a461d3f3b759c0e6c7d9
Brian Dominic Rego <brian.rego03@gmail.com>
2026-01-29 22:59:06 +0530

    Create a refresh service to run on refresh executor pool with a three-second delay

---
+++ src/main/java/com/brian/newfriday/service/ArtistRefreshService.java
@@ -0,0 +1,74 @@
+package com.brian.newfriday.service;
+
+import com.brian.newfriday.client.SpotifyClient;
+import com.brian.newfriday.dtos.ArtistLatestDto;
+import com.brian.newfriday.repository.ArtistRepository;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@Service
+public class ArtistRefreshService {
+    private final ArtistRepository artistRepository;
+    private final SpotifyClient spotifyClient;
+    private final ArtistService artistService;
+
+    private final AtomicBoolean paused = new AtomicBoolean(false);
+    private final AtomicBoolean running = new AtomicBoolean(false);
+
+    public ArtistRefreshService(ArtistRepository artistRepository, SpotifyClient spotifyClient, ArtistService artistService) {
+        this.artistRepository = artistRepository;
+        this.spotifyClient = spotifyClient;
+        this.artistService = artistService;
+    }
+
+    @Async("refreshExecutor")
+    public CompletableFuture<Void> refreshArtistData(){
+        if(!running.compareAndSet(false, true)){
+            return CompletableFuture.completedFuture(null);
+        }
+
+        try{
+            List<ArtistLatestDto> latestArtists = artistRepository.getLatestArtistsWithAlbums();
+            for(ArtistLatestDto artistLatestDto : latestArtists){
+                while(paused.get()){
+                    Thread.sleep(1000);
+                }
+                String artistID = artistLatestDto.getArtistId();
+                String latestAlbumID = artistLatestDto.getLatestAlbumId();
+                String apiLatestAlbumID = spotifyClient.getLatestAlbumByArtistSpotifyId(artistID);
+                Thread.sleep(1*3*1000);//3 sec break
+                if(apiLatestAlbumID==null || apiLatestAlbumID.equals(latestAlbumID)){
+                    continue;
+                }
+                artistService.getCompleteArtist(artistID);
+
+            }
+            return CompletableFuture.completedFuture(null);
+        }catch (Exception e){
+            //log.error("Refresh failed", e);
+            return CompletableFuture.completedFuture(null);
+        }finally {
+            running.set(false);
+        }
+    }
+
+    public void pause(){
+        paused.set(true);
+    }
+
+    public void resume(){
+        paused.set(false);
+    }
+
+    public Boolean isPaused(){
+        return paused.get();
+    }
+
+    public Boolean isRunning(){
+        return running.get();
+    }
+}
