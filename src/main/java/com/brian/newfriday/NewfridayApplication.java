369805fd03849027c3358e70beb8ef018ed0e4bb
Brian Dominic Rego <brian.rego03@gmail.com>
2026-01-30 01:08:06 +0530

    Added scheduling annotation to main application

--- src/main/java/com/brian/newfriday/NewfridayApplication.java
+++ src/main/java/com/brian/newfriday/NewfridayApplication.java
@@ -8,9 +8,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
 public class NewfridayApplication {
 
 	public static void main(String[] args) {
