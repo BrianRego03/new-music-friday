1b0f78412d0fff1e9759e064d7df04dfed425dee
brianrego03 <brian.rego03@gmail.com>
2026-01-30 17:24:10 +0530

    Initialize logger and log artist refresh data

--- src/main/java/com/brian/newfriday/service/ArtistRefreshService.java
+++ src/main/java/com/brian/newfriday/service/ArtistRefreshService.java
@@ -3,6 +3,8 @@ package com.brian.newfriday.service;
 import com.brian.newfriday.client.SpotifyClient;
 import com.brian.newfriday.dtos.ArtistLatestDto;
 import com.brian.newfriday.repository.ArtistRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +21,8 @@ public class ArtistRefreshService {
     private final AtomicBoolean paused = new AtomicBoolean(false);
     private final AtomicBoolean running = new AtomicBoolean(false);
 
+    private static final Logger log = LoggerFactory.getLogger(ArtistRefreshService.class);
+
     public ArtistRefreshService(ArtistRepository artistRepository, SpotifyClient spotifyClient, ArtistService artistService) {
         this.artistRepository = artistRepository;
         this.spotifyClient = spotifyClient;
@@ -28,37 +32,34 @@ public class ArtistRefreshService {
     @Async("refreshExecutor")
     public CompletableFuture<Void> refreshArtistData(){
         if(!running.compareAndSet(false, true)){
+            log.info("Artist data refresh is already running. Exiting this request.");
             return CompletableFuture.completedFuture(null);
         }
 
         try{
+            log.info("Starting artist data refresh");
             List<ArtistLatestDto> latestArtists = artistRepository.getLatestArtistsWithAlbums();
-            System.out.println("Listed successfully");
-            System.out.println(latestArtists.toString());
             for(ArtistLatestDto artistLatestDto : latestArtists){
                 while(paused.get()){
                     Thread.sleep(1000);
                 }
                 String artistID = artistLatestDto.getArtistId();
-                System.out.println("refreshing Artist ID: " + artistID);
                 String latestAlbumID = artistLatestDto.getLatestAlbumId();
                 String apiLatestAlbumID = spotifyClient.getLatestAlbumByArtistSpotifyId(artistID);
-                System.out.println("Not api rated yet, taking a break");
                 Thread.sleep(1*3*1000);//3 sec break
                 if(apiLatestAlbumID==null || apiLatestAlbumID.equals(latestAlbumID)){
                     continue;
                 }
                 artistService.getCompleteArtist(artistID);
-                System.out.println("next api success");
+                log.info("Refreshed artist data for artist ID: " + artistID);
 
             }
             return CompletableFuture.completedFuture(null);
         }catch (Exception e){
-            //log.error("Refresh failed", e);
-            System.out.println("Error in refreshArtistData");
+            log.error("Refresh failed", e);
             return CompletableFuture.completedFuture(null);
         }finally {
-            System.out.println("Artist refresh complete");
+            log.info("Artist data refresh completed");
             running.set(false);
         }
     }
